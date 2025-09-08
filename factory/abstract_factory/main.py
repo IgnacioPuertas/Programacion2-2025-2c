fc59fe1c5171cc9fc9b905ea67f32337b3dce216
Ayelen <alscafati@estudiantes.unsam.edu.ar>
2025-09-08 14:19:26 -0300

    Update main.py
    Se vuelve al main original.

--- factory/abstract_factory/main.py
+++ factory/abstract_factory/main.py
@@ -4,8 +4,6 @@ def main():
     ny = NYPizzaStore(); chi = ChicagoPizzaStore()
     ny.order_pizza("cheese")
     chi.order_pizza("clam")
-    chi.order_pizza("veggie")
-
 
 if __name__ == "__main__":
     main()
